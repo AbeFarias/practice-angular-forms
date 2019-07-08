b1234626d3a3868a146b235e12fb0af0201561ab
Abraham Farias <afarias@Abrahams-MacBook-Air.local>
2019-07-08 16:14:17 -0700

    implemented valid/invalid input fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +8,22 @@ import { FormGroup } from '@angular/forms';
 })
 export class AppComponent implements OnInit{
   title = 'angular-form-practice';
-  accountType: ['Personal', 'Business'];
-  singupForm: FormGroup;
+  accountType = ['Personal', 'Business'];
+  signupForm: FormGroup;
 
   ngOnInit() {
-    
+    this.signupForm = new FormGroup({
+      'accountType': new FormControl('Personal', Validators.required),
+      'name': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'streetAddress': new FormControl(null, Validators.required),
+      'city': new FormControl(null, Validators.required),
+      'country': new FormControl(null, Validators.required),
+      'zipCode': new FormControl(null, [Validators.pattern("[0-9]*"), Validators.maxLength(6)]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
   }
 }
