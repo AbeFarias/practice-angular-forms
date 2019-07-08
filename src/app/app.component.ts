e186a39d18d1ef693045b1e4204dbc1a7e842939
Abraham Farias <afarias@Abrahams-MacBook-Air.local>
2019-07-08 16:26:25 -0700

    status change subscriptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,6 +21,16 @@ export class AppComponent implements OnInit{
       'country': new FormControl(null, Validators.required),
       'zipCode': new FormControl(null, [Validators.pattern("[0-9]*"), Validators.maxLength(6)]),
     });
+
+    // //Subscribe to value changes
+    // this.signupForm.valueChanges.subscribe(
+    //   (value) => console.log(value)
+    // );
+
+    //Subscribe to status changes
+    this.signupForm.statusChanges.subscribe(
+      (status) => console.log(status)
+    );
   }
 
   onSubmit() {
