7975a8dbccf95e6e66e083176a1f8c5de3e54796
Abraham Farias <afarias@Abrahams-MacBook-Air.local>
2019-07-08 15:34:59 -0700

    imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-form-practice';
   accountType: ['Personal', 'Business'];
+  singupForm: FormGroup;
+
+  ngOnInit() {
+    
+  }
 }
